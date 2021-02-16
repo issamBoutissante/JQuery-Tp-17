4e4bafe63fe8e4c8e5588e99bfcbde00b88423d7
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-02-16 17:42:43 +0100

    doen

---
+++ Exercice 2/Exercice2.js
@@ -0,0 +1,25 @@
+$(function () {
+  $("input:submit").click(function (e) {
+    e.preventDefault();
+    //1.	Lire puis afficher le nom de l'utilisateur.
+    // var nom = $("#nom").val();
+    // alert(nom);
+    //
+    //2.	Lire puis afficher le mot de passe.
+    // var motPass = $("#motPass").val();
+    // alert(motPass);
+    //
+    //3.	Lire puis afficher l'état du bouton radio H. Renvoie true si le bouton est sélectionné, sinon false.
+    //var isChecked=$("#H").prop("checked")
+    //alert(isChecked);
+    //4.	Lire puis afficher l'élément sélectionné dans la liste déroulante.
+    //var element = $("#fonction").val();
+    //alert(element);
+    //5.	Écrit « Kamal » dans la zone de texte Nom d'utilisateur.
+    //$("#nom").val("Kamal");
+    //6.	Sélectionne le bouton radio H
+    //$("#H").prop("checked", true);
+    //7.	Sélectionne Retraité dans la liste déroulante.
+    //$("#fonction").val("Retraite");
+  });
+});
